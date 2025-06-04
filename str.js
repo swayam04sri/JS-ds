a57e358267c3b0959aa1b2dca03d2fa664b8f7ba
LAPTOP-0DL88DTG\swyam <swyam.priyal@gmail.com>
2025-06-04 22:34:38 +0530

    added DS question

---
+++ str.js
@@ -0,0 +1,21 @@
+function lengthOfLongestSubstring(s) {
+    let set = new Set();
+    let left = 0;
+    let maxLength = 0;
+
+    for (let right = 0; right < s.length; right++) {
+        while (set.has(s[right])) {
+            set.delete(s[left]);
+            left++;
+        }
+        set.add(s[right]);
+        maxLength = Math.max(maxLength, right - left + 1);
+    }
+
+    return maxLength;
+}
+
+// Test Cases
+console.log(lengthOfLongestSubstring("abcabcbb")); // 3
+console.log(lengthOfLongestSubstring("bbbbb")); // 1
+console.log(lengthOfLongestSubstring("pwwkew")); // 3
